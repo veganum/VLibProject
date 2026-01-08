a5c41a2740d33ff5f4188a53c9438ae7d6c14710
veganum <veganum@hotmail.com>
2026-01-08 21:25:53 +0100

    correcciones en hightlight

--- projects/vgn/veganum-ngx-components/src/stories/lib/veganum-ngx-components/directives/highlight.stories.ts
+++ projects/vgn/veganum-ngx-components/src/stories/lib/veganum-ngx-components/directives/highlight.stories.ts
@@ -1,4 +1,4 @@
-import { Highlight } from './../../../../../directives/highlight/highlight';
+import { Highlight } from './../../../../directives/highlight/highlight';
 import { Component, Input, signal } from '@angular/core';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 
